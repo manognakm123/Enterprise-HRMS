38c32a4abb3b0ef34169bf514ce4449d29cb010e
unknown <manognakm123>
2026-08-19 10:29:14 +0530

    Clean up generated files and framework artifacts

--- test_logger.py
+++
@@ -1,9 +0,0 @@
-from automation_framework.utilities.logger import get_logger
-
-logger = get_logger()
-
-print("Logger object:", logger)
-
-logger.info("This is a test log message.")
-
-print("Finished")
